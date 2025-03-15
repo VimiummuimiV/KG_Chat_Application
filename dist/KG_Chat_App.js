5bb786964562aa358975e7721aeaa5590df13807
Puncher <puncher@gmail.com>
2025-03-15 18:39:34 +0200

    Up 1.1.1

--- dist/KG_Chat_App.js
+++ dist/KG_Chat_App.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.1.0
+// @version      1.1.1
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
