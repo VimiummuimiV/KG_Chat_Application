6edfa908c7d1b6210bba3dc86126bf390493e9e1
Puncher <puncher@gmail.com>
2025-03-15 22:59:37 +0200

    Added smooth transition margin-bottom for the messages-panel

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.1.7
+// @version      1.1.8
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
