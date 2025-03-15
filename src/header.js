5d662a03172cf6717faec2ad4a11567c0a20fb55
Puncher <puncher@gmail.com>
2025-03-16 01:44:18 +0200

    Better media css queries for the max-width 768px

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.3.9
+// @version      1.4.0
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
