4e2b0a16b3d594430eee4b8a4a5519242d2779a1
Puncher <puncher@gmail.com>
2025-03-16 02:39:25 +0200

    Do not block dimming-element and scaled-thumbnail

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.4.7
+// @version      1.4.8
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
