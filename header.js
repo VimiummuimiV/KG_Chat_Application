4a3279d7a50b289a25950004dac03061f40f95ed
Puncher <puncher@gmail.com>
2025-03-20 21:55:45 +0200

    Build

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.9.8
+// @version      1.9.9
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
