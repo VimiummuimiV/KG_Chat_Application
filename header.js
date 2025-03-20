bef8645a009d40af3a9d649001fc0e7fab7898a7
Puncher <puncher@gmail.com>
2025-03-20 12:27:56 +0200

    Vesion up 1.7.9

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      1.7.8
+// @version      1.7.9
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
