0223f1adabcb75ccbda341a0cb2ddf72ed6599fb
Puncher <puncher@gmail.com>
2025-03-26 19:48:36 +0200

    Fixed emoji font properties for emoji panel

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      2.3.4
+// @version      2.3.5
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
