b0ad5a5a0e773d5bfa088526823bcfa62432f550
Puncher <puncher@gmail.com>
2025-03-28 13:59:43 +0200

    Removed shadow from youtube thumbnail.

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      2.4.7
+// @version      2.4.8
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
