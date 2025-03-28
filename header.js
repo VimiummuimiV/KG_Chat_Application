992659e272213ab0aac38b3c0425c9a3d56c6cd2
Puncher <puncher@gmail.com>
2025-03-28 13:55:12 +0200

    slight box shadow to image and video small elements

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      2.4.6
+// @version      2.4.7
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
