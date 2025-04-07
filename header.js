414bc8a4afdeedae000233dd27de469d1e1c3278
Puncher <puncher@gmail.com>
2025-04-07 03:36:31 +0300

    Fix. Text base message inline-flex to make the text flex start.

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      3.0.9
+// @version      3.1.0
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
