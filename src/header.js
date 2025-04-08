cf7f2c0543061090f5251cceadb9ba51e032101b
Puncher <puncher@gmail.com>
2025-04-08 22:39:11 +0300

    emoji smaller size. 1.25em instead 1.5em. Less horizontal space.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      3.1.9
+// @version      3.2.1
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
