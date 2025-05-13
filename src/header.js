25cea19bb92ea20b3674bb4214f4f90702eac1db
Puncher <puncher@gmail.com>
2025-05-13 06:21:51 +0300

    - Version 4.2.5 - Fix for the img (emoticon case scaled vertically abnormally)

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      4.2.4
+// @version      4.2.5
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
