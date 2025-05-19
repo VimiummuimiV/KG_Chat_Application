a8d54e273189c2277b15e35f37dd0cc005c2db65
Puncher <puncher@gmail.com>
2025-05-19 07:23:37 +0300

    Bump version to 4.5.7 and change button position from absolute to fixed in styles

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      4.5.6
+// @version      4.5.7
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
