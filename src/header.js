3333d66adf9f2624198a587c2dbc5ea13220f22b
Puncher <puncher@gmail.com>
2025-05-20 16:51:30 +0300

    Bump version to 4.7.3

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      4.7.2
+// @version      4.7.3
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
