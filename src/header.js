04d9c7e65c88aee53459f273c202718c1ed5c9ce
Puncher <puncher@gmail.com>
2025-05-27 01:47:32 +0300

    Update version to 5.1.4 and enhance ignore options popup layout

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      5.1.2
+// @version      5.1.4
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
