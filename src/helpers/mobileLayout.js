5992b826211cdb0de2570e16e0674918119a046e
Puncher <puncher@gmail.com>
2025-07-13 03:28:12 +0300

    height 0

--- src/helpers/mobileLayout.js
+++ src/helpers/mobileLayout.js
@@ -11,6 +11,7 @@ export function handleMobileLayout(chatContainer, chatContent, messagesPanel, dr
     globalMobileStyles.textContent = `
       html, body {
         overflow: hidden !important;
+        height: 0 !important;
       }
       #app-chat-container .emoji-panel {
         transform: translate(-50%, 0%) !important;
