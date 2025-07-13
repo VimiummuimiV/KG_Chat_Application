cd925a5e50086c6daf0e403a94935b9d3b507cbc
Puncher <puncher@gmail.com>
2025-07-13 03:06:14 +0300

    viewportMeta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -218,7 +218,7 @@ export function addViewportMeta() {
   if (!document.querySelector('meta[name="viewport"]')) {
     const viewportMeta = document.createElement('meta');
     viewportMeta.name = 'viewport';
-    viewportMeta.content = 'width=device-width, initial-scale=1.0';
+    viewportMeta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
     document.head.appendChild(viewportMeta);
     console.log('Viewport meta tag added dynamically');
   }
