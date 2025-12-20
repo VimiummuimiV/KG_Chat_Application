810e4289c9a2571158208c20ec74da8a5f1849eb
Aboba <blahblahmail.com>
2025-12-20 21:11:24 +0200

    I made a 'mixin' side button with different options. I also improved the position of the additional chat toggle button on mobile devices.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_Chat_Application
 // @namespace    klavogonki
-// @version      5.3.3
+// @version      5.3.4
 // @description  Enhance the chat abilities
 // @author       Patcher
 // @match        *://klavogonki.ru/*
